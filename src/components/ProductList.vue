c73738d43f945dccbfc4e182effc37bd3abf7297
Evgeny-Laznevoy <laznevoy86@gmail.com>
2021-02-08 19:56:49 +0500

    layout product list

---
+++ src/components/ProductList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto mb-2 mt-2"
+      outlined
+      v-for="(product,i) in products"
+      :key="i"
+    >
+      <v-container fluid>
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <div>
+            <v-card-title>
+              {{ product.name }}
+            </v-card-title>
+            <v-card-subtitle>
+              {{ product.description }}
+            </v-card-subtitle>
+          </div>
+          <span>
+              {{ product.price }}
+          </span>
+        </div>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "productList",
+  computed: {
+    ...mapState(["products"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/store.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <div>
+      <v-toolbar dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Sports Store</v-toolbar-title>
+      </v-toolbar>
+    </div>
+
+    <v-main>
+      <v-card class="grey lighten-4 mt-6">
+        <v-card>
+          <v-row no-gutters style="height: 40px">
+            <v-col style="width:50%" v-for="row in rows" :key="row" :align-self="row">
+              <v-card class="pa-2" outlined title>
+                {{ row }}
+                <div v-if="row == 'Products'">
+                <productList />
+              </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import productList from "./ProductList";
+export default {
+  name: "store",
+  components: {
+    productList,
+  },
+  data() {
+    return {
+      rows: ["Categories", "Products"],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
